52f73f8b02b1a4f79e03c1c37f48164eb5626557
Vasil Vangelovski <vv@atomidata.com>
2014-04-10 10:38:32 +0200

    Added Vagrantfile and bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get upgrade
+sudo apt-get -y install software-properties-common python-software-properties
+
+#Oracle java
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get -y update
+/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java6-installer oracle-java6-set-default
+
+
+apt-get install -y git vim wget screen curl gnupg flex bison gperf build-essential
+apt-get install -y zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev
+apt-get install -y libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386
+apt-get install -y libgl1-mesa-dev g++-multilib mingw32 tofrodos
+apt-get install -y python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+
+mkdir /home/vagrant/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > /home/vagrant/bin/
+chmod a+x ~/bin/repo
