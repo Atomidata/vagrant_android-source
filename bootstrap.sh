a9703594a3a9444da36ee45f9440da6e3bfaea57
Vasil Vangelovski <vvangelovski@gmail.com>
2014-04-10 22:44:33 +0200

    changed java to openjdk7, need to make the java version a choice for older builds

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,12 @@ apt-get upgrade
 sudo apt-get -y install software-properties-common python-software-properties
 
 #Oracle java
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get -y update
-/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-sudo apt-get install -y oracle-java6-installer oracle-java6-set-default
+#sudo add-apt-repository -y ppa:webupd8team/java
+#sudo apt-get -y update
+#/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+#sudo apt-get install -y oracle-java7-installer oracle-java7-set-default
+
+sudo apt-get -y install openjdk-7-jdk openjdk-7-jre
 
 
 apt-get install -y git vim wget screen curl gnupg flex bison gperf build-essential
